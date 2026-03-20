c972aa6a137f2f4192a581805090395137abd76f
Harshit Mani Tripathi <harshit@hudater.dev>
2026-03-20 23:06:08 +0530

    add toggle button for h5t-talwinder; add css-reset file which is applied at startup

--- chai-h5t.js
+++ chai-h5t.js
@@ -1,4 +1,4 @@
-let talwinderActive = false;
+let talwinderActive = true;
 
 function toggleTalwinder() {
     if (!talwinderActive) {
@@ -11,9 +11,29 @@ function toggleTalwinder() {
 }
 
 function applyStyles() {
+
+    // load reset stylesheet
+    let head = document.getElementsByTagName('HEAD')[0];
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'css-reset.css';
+    head.appendChild(link);
+    document.getElementById("h5t-talvinder-status").textContent = "Active"
     console.log("ON")
 }
 
 function removeStyles() {
+    const stylesheet = document.querySelector('link[href*="css-reset.css"]');
+
+    // Check if the element exists before attempting removal
+    if (stylesheet) {
+        stylesheet.remove();
+    }
+    document.getElementById("h5t-talvinder-status").textContent = "Inactive"
+    // document.querySelector('link[href*="css-reset.css"]').remove()
     console.log("OFF")
+    // alert("!");
 }
+
+applyStyles();
